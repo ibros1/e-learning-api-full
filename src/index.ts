1211f66eb94d14e63a77523f32ce35bb4e72b940
ibros1 <maxamedsuhayb41@gmail.com>
2025-08-18 10:24:38 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,9 @@ const app = express();
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+// To:
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT;
 app.use(
